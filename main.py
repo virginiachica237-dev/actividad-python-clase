a3a41f913f9b0b1eb9958e89a52d09922775a20a
virginiachica237-dev <virginiachica237@gmail.com>
2026-06-09 15:19:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from mascota import Mascota
+
+# Crear objetos de la clase Mascota
+mascota1 = Mascota("Firulais", "Perro", 5)
+mascota2 = Mascota("Mishi", "Gato", 3)
+
+# Mostrar información y ejecutar métodos
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
